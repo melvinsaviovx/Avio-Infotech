1492e3a726acfd6e325ea45ca4caf191df713d0f
Melvin Savio <melvinsaviovx@gmail.com>
2025-09-06 20:01:11 +0530

    footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,9 +5,10 @@ import { Home } from "./components/home/home";
 import { About } from './components/about/about';
 import { Services } from './components/services/services';
 import { Contact } from './components/contact/contact';
+import { Footer } from "./components/footer/footer";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home, About,Services,Contact],
+  imports: [RouterOutlet, Header, Home, About, Services, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
