1b7374c717ef0e596a6519681105daec7ffc2f8d
Melvin Savio <melvinsaviovx@gmail.com>
2025-09-07 11:37:15 +0530

    avio infotech

---
+++ src/app/components/founders/founders.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Founders } from './founders';
+
+describe('Founders', () => {
+  let component: Founders;
+  let fixture: ComponentFixture<Founders>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Founders]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Founders);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/founders/founders.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-founders',
+  imports: [],
+  templateUrl: './founders.html',
+  styleUrl: './founders.css'
+})
+export class Founders {
+
+}
